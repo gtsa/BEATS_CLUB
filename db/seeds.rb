a72a3ff4f4ee9ed441e8cb2cf0d9c41cc72ccdfe
George Tsagiannis <gtsagiannis@gmail.com>
2022-11-29 13:16:27 +0000

    seeds for users, profiles, genres (before model)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# require "faker"
+
+names = [
+  ["George", "Tsagiannis"],
+  ["Rajul", "Nail"],
+  ["Debora", "Faccinello"],
+  ["Gabriel","Wilkinson"],
+  ["Alex", "McKenzie"]
+]
+5.times do
+  names << [Faker::Name.first_name, Faker::Name.last_name  ]
+end
+
+# user_tester_george
+User.create(email: 'george@email.com', password: '111111')
+# user_tester_rajul
+User.create(email: 'rajul@email.com', password: '111111')
+# user_tester_debora
+User.create(email: 'debora@email.com', password: '111111')
+# user_tester_gabriel
+User.create(email: 'gabriel@email.com', password: '111111')
+# user_tester_alex
+User.create(email: 'alex@email.com', password: '111111')
+
+names[5..].each do |name|
+  User.create(
+    email: Faker::Internet.safe_email(name: name[0]),
+    password: Faker::Internet.password(min_length: 8)
+  )
+end
+
+users = User.all.to_a
+
+30.times do
+  Genre.create(
+    name: Faker::Music.genre
+  )
+end
+
+(0..9).each do |i|
+  Profile.create(
+    user_id: users[i].id,
+    first_name: names[i][0],
+    last_name: names[i][1],
+    nickname: Faker::Internet.username(specifier: names[i].join(' '), separators: %w[. _ -]),
+    bio: Faker::Lorem.paragraph(sentence_count: (3..6).to_a.sample),
+    location: Faker::Address.city
+  )
+end
