e20f3db2814a4fc7345848420c3e7b03ab46eae9
George Tsagiannis <gtsagiannis@gmail.com>
2022-11-30 13:35:20 +0000

    Corection

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,7 @@
 class Community < ApplicationRecord
   belongs_to :genre
   has_many :profiles, through: :join_communities
+  has_many :posts
   validates :name, presence: true, length: { minimum: 6 }
   validates :description, presence: true, length: { minimum: 25 }
 end
