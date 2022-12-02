88d82fb0ce7f17468e54747fcb1e0defbe0a0b2b
George Tsagiannis <gtsagiannis@gmail.com>
2022-12-02 19:12:48 +0000

    corrected redundant sql queries on profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,8 @@ class PagesController < ApplicationController
   end
 
   def myprofile
-    # redirect_to profile_path(current_user.id) if user_signed_in?
-    @profile = Profile.find_by(user: current_user)
+    @profile = current_user.profiles.first
     @user_check = current_user == @profile.user
+    raise
   end
 end
