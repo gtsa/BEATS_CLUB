ca34921aad897dd3a383a9870438208dc0e680aa
debora fachinello <deborafachi@hotmail.com>
2022-12-05 12:12:55 +0000

    tabs completed

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,7 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tabs"
 export default class extends Controller {
+  static targets = ["created", "joined", "createdLink", "joinedLink"]
   connect() {
-    console.log("Hello from tabs controller")
+    console.log(this.createdTarget)
+  }
+
+  displayCreated() {
+    this.createdTarget.classList.remove("d-none")
+    this.joinedTarget.classList.add("d-none")
+
+    this.createdLinkTarget.classList.add("active")
+    this.joinedLinkTarget.classList.remove("active")
+  }
+
+  displayJoined() {
+    this.joinedTarget.classList.remove("d-none")
+    this.createdTarget.classList.add("d-none")
+
+    this.joinedLinkTarget.classList.add("active")
+    this.createdLinkTarget.classList.remove("active")
   }
 }
